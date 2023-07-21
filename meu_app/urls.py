7b35ec87576036ed7fb542de1d55b8f2330fe210
isacostaf <iisabellecostaf@gmail.com>
2023-07-21 11:49:43 -0300

    working

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.login_or_create_account, name='login_or_create_account'),
     path('welcome/', views.welcome, name='welcome'),
     path('remove_task/<int:task_id>/', views.remove_task, name='remove_task'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    path('task_list/', views.task_list, name='task_list'),
 ]
--- meu_app/views.py
+++ meu_app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Task
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from .forms import TaskForm
 
 
 def login_or_create_account(request):
@@ -43,3 +44,20 @@ def remove_task(request, task_id):
     if request.method == 'POST':
         task.delete()
     return redirect('welcome')
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('welcome')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'edit_task.html', {'form': form})
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
